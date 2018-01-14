929b1b16d380f5ff0f36dbcbb0bb87a0900b5032
Spenzor <spencercorwin@mac.com>
2018-01-14 12:29:39 -0800

    Create TheHighestProfitWins.py

---
+++ TheHighestProfitWins.py
@@ -0,0 +1,7 @@
+#Codewars Challenge: The highest profit wins!
+#Simply returns the minimum and maximum number of the given list/array
+def min_max(lst):
+  min_val = min(lst)
+  max_val = max(lst)
+  new_lst = [min_val,max_val]
+  return new_lst
