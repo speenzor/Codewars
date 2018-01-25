9a78e0519553f94f52c11424b2088ac9c03d4c0a
Spenzor <spencercorwin@mac.com>
2018-01-24 21:44:19 -0800

    Update Scramblies.py

--- Scramblies.py
+++ Scramblies.py
@@ -2,7 +2,18 @@
 #This function returns true if a portion of s1 characters can be rearranged to match s2,
 #otherwise it returns False.
 
-#This is the slow way that works but is too slow for Codewars:
+#This way works fast enough for Codewars but was still not as fast as it could be, I suspect:
+def scramble(s1, s2):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for i in alphabet:
+        if s2.count(i) <= s1.count(i):
+            continue
+        else:
+            return False
+    return True
+
+
+#This is the super slow way that works but is too slow for Codewars and for very long strings:
 def scramble2(s1,s2):
     s1_list = list(s1)
     match_list = []
@@ -12,11 +23,17 @@ def scramble2(s1,s2):
             s1_list.remove(i)
     new_match_list = ''.join(match_list)
     if new_match_list == s2:
-        return (True)
+        return True
     else:
-        return (False)
-        
-        
-        
-        
-#This is a faster way that Codewars accepts:
+        return False
+
+
+#This way works and is fast except it doesn't work for double letters
+def scramble3(s1,s2):
+    s2_list = list(s2)
+    filtered_list = filter(lambda x: x in s1, s2_list)
+    new_match_string = ''.join(filtered_list)
+    if new_match_string == s2:
+        print True
+    else:
+        print False
